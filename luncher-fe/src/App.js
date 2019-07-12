14eb11f8f4f07a1cd4dc11f0a5530a110ec06ff4
Dominic Torruellas <dom@domtorr.com>
2019-07-11 17:08:58 -0700

    Added NavBar to App.js - Put nav tag in NavBar.js

--- luncher-fe/src/App.js
+++ luncher-fe/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import NavBar from './components/NavBar';
+
 function App() {
   return (
     <div>
-      <div />
+      <NavBar />
     </div>
   );
 }
--- luncher-fe/src/components/NavBar/NavBar.js
+++ luncher-fe/src/components/NavBar/NavBar.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div>
+    <nav>
       <div>NavBar</div>
-    </div>
+    </nav>
   );
 };
 
