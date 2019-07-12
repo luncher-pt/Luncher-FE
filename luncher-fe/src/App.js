c8d551135fe43ea5f9465795784220378d393ef6
Dominic Torruellas <dom@domtorr.com>
2019-07-11 17:15:22 -0700

    Added Router and first route to Home component

--- luncher-fe/src/App.js
+++ luncher-fe/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div>
-      <NavBar />
-    </div>
+    <main>
+      <Router>
+        <NavBar />
+        <div>
+          <Route exact path="/" component={Home} />
+        </div>
+      </Router>
+    </main>
   );
 }
 
