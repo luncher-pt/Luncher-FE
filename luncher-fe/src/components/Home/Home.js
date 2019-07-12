cdbf9f6ca965088b6f70b6a3b1c37833ee23dde4
Shobhita Vaishnav <to.shobhita@gmail.com>
2019-07-12 19:10:41 +0530

    Adding initial code for Home page

--- luncher-fe/src/components/Home/Home.js
+++ luncher-fe/src/components/Home/Home.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { fetchingSchoolsAction , deletingSchoolAction } from '../actions';
+//Todo: replace with real component when ready
+import School from "./School";
+import "./School.css";
 
-const Home = () => {
-  return (
-    <div>
-      <div>Home</div>
-    </div>
-  );
-};
+class Home extends React.Component {
+  componentDidMount() {
+    this.props.fetchingSchoolsAction();
+  }
 
-export default Home;
+  deleteHandler = id => {
+    this.props
+      .deletingSchoolAction(id)
+      .then(() => this.props.history.push("/"));
+  }
+
+  editHandler = school => {
+    this.props.history.push("/updateSchool", {school : school} );
+  }
+
+  render() { 
+    return (
+      <div className="SchoolList">
+        {
+          !this.props.fetchingSchools 
+          ? this.props.schools.map(school => (<School key={school.id} school={school} editHandler={this.editHandler} deleteHandler={this.deleteHandler} />))
+          : <h4>Fetching schools ...</h4>
+        }
+        {
+          this.props.error && <p className="error"> { 
+            this.props.error
+          } </p> 
+        }
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ schools , fetchingSchools , error }) => ({
+  schools,
+  fetchingSchools,
+  error
+});
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { fetchingSchoolsAction , deletingSchoolAction }
+  )(Home)
+);
