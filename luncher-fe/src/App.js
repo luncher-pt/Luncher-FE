3b6051137586e1a6fa5ff7ceb9d101edf3065af3
Dominic Torruellas <dom@domtorr.com>
2019-07-11 17:20:58 -0700

    Added Admin component route

--- luncher-fe/src/App.js
+++ luncher-fe/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
+import Admin from './components/Admin/Admin';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <NavBar />
         <div>
           <Route exact path="/" component={Home} />
+          <Route path="/admin" component={Admin} />
         </div>
       </Router>
     </main>
