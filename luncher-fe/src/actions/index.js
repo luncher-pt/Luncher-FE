98ecd70c9ef50b2111f7add91213b683b9abc51e
Dominic Torruellas <dom@domtorr.com>
2019-07-15 21:54:27 -0700

    initial register action creator

--- luncher-fe/src/actions/index.js
+++ luncher-fe/src/actions/index.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
-export const LOGGING_IN = "LOGGING_IN";
-export const LOGGING_IN_SUCCESS = "LOGGING_IN_SUCCESS";
-export const LOGGING_IN_FAILURE = "LOGGING_IN_FAILURE";
+export const LOGGING_IN = 'LOGGING_IN';
+export const LOGGING_IN_SUCCESS = 'LOGGING_IN_SUCCESS';
+export const LOGGING_IN_FAILURE = 'LOGGING_IN_FAILURE';
+
+export const REGISTERING = 'REGISTERING';
+export const REGISTERING_SUCCESS = 'REGISTERING_SUCCESS';
+export const REGISTERING_FAILURE = 'REGISTERING_FAILURE';
 
 export const FETCHING_SCHOOLS = 'FETCHING_SCHOOLS';
 export const FETCHING_SCHOOLS_SUCCESS = 'FETCHING_SCHOOLS_SUCCESS';
@@ -20,18 +24,27 @@ export const UPDATING_SCHOOL = 'UPDATING_SCHOOL';
 export const UPDATING_SCHOOL_SUCCESS = 'UPDATING_SCHOOL_SUCCESS';
 export const UPDATING_SCHOOL_FAILURE = 'UPDATING_SCHOOL_FAILURE';
 
+const API_URL = 'http://luncher-lambda-buildweek.herokuapp.com';
+
 export const loggingInAction = creds => dispatch => {
-    dispatch({ type: LOGGING_IN });
-    return axios
-                .post("http://luncher-lambda-buildweek.herokuapp.com/login", creds)
-                .then(resp => {
-                        localStorage.setItem("token", resp.data.payload);
-                        dispatch({type: LOGGING_IN_SUCCESS});
-                    }
-                )
-                .catch(err => 
-                  dispatch({type: LOGGING_IN_FAILURE, error: err.message}) 
-                );
+  dispatch({ type: LOGGING_IN });
+  return axios
+    .post('http://<TBD>:<TBD>/login', creds)
+    .then(resp => {
+      localStorage.setItem('token', resp.data.payload);
+      dispatch({ type: LOGGING_IN_SUCCESS });
+    })
+    .catch(err => dispatch({ type: LOGGING_IN_FAILURE, error: err.message }));
+};
+
+export const registeringAction = creds => dispatch => {
+  dispatch({ type: REGISTERING });
+  return axios
+    .post(`${API_URL}/register`, creds)
+    .then(resp => {
+      dispatch({ type: REGISTERING_SUCCESS });
+    })
+    .catch(err => dispatch({ type: REGISTERING_FAILURE, error: err.message }));
 };
 
 export const fetchingSchoolsAction = () => dispatch => {
@@ -77,9 +90,15 @@ export const deletingSchoolAction = id => dispatch => {
 export const updatingSchoolAction = updatedSchool => dispatch => {
   dispatch({ type: UPDATING_SCHOOL });
   return axios
-    .put(`http://luncher-lambda-buildweek.herokuapp.com/schools/${updatedSchool.id}`, updatedSchool, {
-      headers: { Authorization: localStorage.getItem('token') },
-    })
+    .put(
+      `http://luncher-lambda-buildweek.herokuapp.com/schools/${
+        updatedSchool.id
+      }`,
+      updatedSchool,
+      {
+        headers: { Authorization: localStorage.getItem('token') },
+      }
+    )
     .then(resp =>
       dispatch({ type: UPDATING_SCHOOL_SUCCESS, payload: resp.data })
     )
