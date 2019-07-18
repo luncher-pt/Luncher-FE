0c26e4db87179b155eabc115ba7326524212a6ff
Dominic Torruellas <dom@domtorr.com>
2019-07-18 11:25:42 -0700

    auto format

--- luncher-fe/src/components/NavBar/NavBar.js
+++ luncher-fe/src/components/NavBar/NavBar.js
@@ -5,8 +5,22 @@ import './NavBar.css';
 const NavBar = () => {
   return (
     <div className="NavBar">
-      <NavLink exact={true} to="/" className="inactive" activeClassName="active">Home</NavLink>
-      <NavLink exact={true} to="/login" className="inactive" activeClassName="active">Login</NavLink> 
+      <NavLink
+        exact={true}
+        to="/"
+        className="inactive"
+        activeClassName="active"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        exact={true}
+        to="/login"
+        className="inactive"
+        activeClassName="active"
+      >
+        Login
+      </NavLink>
     </div>      
   );
 };
