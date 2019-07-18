69b7e9bd8b5e430eb960bd5953e2a859a8a89061
Dominic Torruellas <dom@domtorr.com>
2019-07-18 11:13:14 -0700

    Changed admin page to profile page

--- luncher-fe/src/App.js
+++ luncher-fe/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
-import Admin from './components/Admin/Admin';
+import Profile from './components/Profile/Profile';
 import Login from './components/Login/Login';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <NavBar />
         <div>
           <Route exact path="/" component={Home} />
-          <Route path="/admin" component={Admin} />
+          <Route path="/profile" component={Profile} />
           <Route path="/login" component={Login} />
           {/* single school route ( /updateSchool ) to be implemented */}
         </div>
