88418df36e0c9e529eab3d0fd85a127f5900698d
Dominic Torruellas <dom@domtorr.com>
2019-07-18 14:58:11 -0700

    displaying schools owned by user on their profile page

--- luncher-fe/src/components/Profile/Profile.js
+++ luncher-fe/src/components/Profile/Profile.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { fetchingSchoolsAction } from '../../actions';
+import School from '../School/School';
 
 const Admin = () => {
+  const { userId, schools, isLoggedIn, fetchingSchools, error } = useSelector(
+    state => state
+  );
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (schools.length === 0) {
+      dispatch(fetchingSchoolsAction());
+    }
+  }, [schools]);
+
+  const mySchools = schools.filter(school => school.admin_id === userId);
+
   return (
     <div>
       <div>Admin</div>
+      <div>My Schools</div>
+      <div>
+        {!fetchingSchools ? (
+          mySchools.map(school => (
+            <School
+              key={school.id}
+              school={school}
+              // editHandler={this.editHandler}
+              // deleteHandler={this.deleteHandler}
+              isLoggedIn={isLoggedIn}
+            />
+          ))
+        ) : (
+          <h4 className="Message">Fetching school information ...</h4>
+        )}
+        {error && <p className="error"> {error} </p>}
+      </div>
+      <div>Profile Information</div>
     </div>
   );
 };
